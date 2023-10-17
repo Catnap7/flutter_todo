643577d1a07434c55ce738ad67fc9c3246c70a9e
JaeWooJang <jjw33063@gmail.com>
2023-10-18 02:56:42 +0900

    utils 캘린더 이벤트 관련 함수 추가
    이벤트 날짜, 범위 지정 함수 추가

--- lib/common/util/utill.dart
+++ lib/common/util/utill.dart
@@ -1,4 +1,7 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
  showSnackBar(BuildContext context, String message) {
   return ScaffoldMessenger.of(context).showSnackBar(
@@ -37,3 +40,36 @@ String getDifferenceDate(DateTime date) {
     return 'D-Day';
   }
 }
+
+class CalendarEvent {
+  final String title;
+
+  const CalendarEvent(this.title);
+
+  @override
+  String toString() => title;
+}
+
+final kEvents = LinkedHashMap<DateTime, List<CalendarEvent>>(
+  equals: isSameDay,
+  hashCode: getHashCode,
+)..addAll(_kEventSource);
+
+final _kEventSource = Map.fromIterable(List.generate(50, (index) => index),
+    key: (item) => DateTime.utc(kFirstDay.year, kFirstDay.month, item * 5),
+    value: (item) => List.generate(
+        item % 4 + 1, (index) => CalendarEvent('Event $item | ${index + 1}')))
+  ..addAll({
+    kToday: [
+      CalendarEvent('Today\'s Event 1'),
+      CalendarEvent('Today\'s Event 2'),
+    ],
+  });
+
+int getHashCode(DateTime key) {
+  return key.day * 1000000 + key.month * 10000 + key.year;
+}
+
+final kToday = DateTime.now();
+final kFirstDay = DateTime(kToday.year, kToday.month - 3, kToday.day);
+final kLastDay = DateTime(kToday.year, kToday.month + 3, kToday.day);
--- lib/screen/main/tab/calendar/calendar_fragment.dart
+++ lib/screen/main/tab/calendar/calendar_fragment.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todolist/common/util/utill.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class CalendarFragment extends StatefulWidget {
@@ -9,9 +10,22 @@ class CalendarFragment extends StatefulWidget {
 }
 
 class _CalendarFragmentState extends State<CalendarFragment> {
+  late final ValueNotifier<List<CalendarEvent>> _selectedEvents;
   CalendarFormat _calendarFormat = CalendarFormat.month;
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
+
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedEvents = ValueNotifier(_getEventsForDay(_focusedDay));
+  }
+
+  List<CalendarEvent> _getEventsForDay(DateTime day) {
+    // Implementation example
+    return kEvents[day] ?? [];
+  }
 
   @override
   Widget build(BuildContext context) {
